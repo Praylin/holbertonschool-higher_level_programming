5cbed2c621a3c970c746abb0b3d994b5a6ddb494
Praylin Diana Dinamoni <36@p2-1.hbtn.io>
2016-02-06 12:23:57 -0800

    Edit the calculation mistake of price

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -6,7 +6,7 @@ print "Welcome to the taxes and tip calculator!" #Print the welcome message
 meal_price = float(raw_input ("What is the price before tax? ")) #Get price of meal before tax from user
 taxes = float(raw_input("What are the taxes? (in %) ")) #Get the % of tax from user
 tip = float(raw_input("What do you want to tip?  (in %) ")) #Get the % of tip the user wish to pay
-meal_price = meal_price + meal_price * taxes #Price of meal after tax
-total_price = meal_price + meal_price * tip  #Price of meal after adding the tip, ie, Total price
-print("The price you need to pay is: %f." %total_price) #Print total price
+meal_price = meal_price + (meal_price * tip/100) #Price of meal after adding tip
+total_price = meal_price + (meal_price * taxes/100) #Total price after adding tax
+print("The price you need to pay is: $%f." %total_price) #Print total price
 
