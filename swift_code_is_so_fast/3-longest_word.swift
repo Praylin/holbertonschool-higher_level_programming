b2fed6ce402b3d047c5040db1b64f50b760f9694
praylin <rampoche@macbooks-MacBook-Pro.local>
2016-05-23 22:39:45 -0700

    Find the first longest word in a sentence

---
+++ swift_code_is_so_fast/3-longest_word.swift
@@ -0,0 +1,21 @@
+//find the first longest word in a text
+func longest_word(text: String) -> (String) {
+  var word = ""
+  var length = 0
+  var max = 0
+  var longestWord = ""
+  for character in text.characters {
+    if character == " " {
+      if length > max {
+          max = length
+          longestWord = word
+      }
+      word = ""
+      length = 0
+  } else {
+      word += "\(character)"
+      length = length + 1
+  }
+}
+return longestWord
+}
---
+++ swift_code_is_so_fast/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+c > 3
+c >= 3
+c > 4
+c < 4
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+"Your name is \(name1)"
