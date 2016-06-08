17d5c607afa85f423d8ace15bb4a6d74d055af83
praylin <rampoche@macbooks-MacBook-Pro.local>
2016-06-07 17:02:09 -0700

    Fixed the problems in displaying distinct last_name

--- basic_sql_playground/5-main.sql
+++ basic_sql_playground/5-main.sql
@@ -1,5 +1,9 @@
 /*all distinct last_name of the TVShow = Game of Thrones ordered by last_name ascending*/
-select last_name from Person where id = (select person_id from TVShowPerson where tvshow_id = (select id from TVShow where name = 'Game of Thrones'));
+select distinct last_name from Person LEFT JOIN TVShowPerson
+on Person.id = TVShowPerson.person_id
+LEFT JOIN TVShow
+on TVShowPerson.tvshow_id = TVShow.id
+where name = 'Game of Thrones' order by last_name;
 /*the number of Person where the age is greater than 30*/
 select count(*) from Person where age > 30;
 /*all Person records with all information from all tables*/
